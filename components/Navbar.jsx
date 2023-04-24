05f45bfd859a2d0eb9c8cbebeec4de5912618dd7
Aneta <sopaaneta@gmail.com>
2023-04-24 14:26:31 +0200

    Changed the name on the page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar({ router }) {
     <nav className="navbar">
       <div className="container">
         <Link href="/" className="logo">
-          Lutfhi.
+          Butterfly.
         </Link>
 
         <ul className="nav-links">
